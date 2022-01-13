54de97b57f0bc5872987c6140f4f86b3373cd57d
Dóró Szilárd <szilard.doro@bishop.hu>
2022-01-13 18:34:01 +0100

    initial commit

---
+++ apps/react-md-editor-next/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'react-md-editor-next',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/react-md-editor-next',
+};
